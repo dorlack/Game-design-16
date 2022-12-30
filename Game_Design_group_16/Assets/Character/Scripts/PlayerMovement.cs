c3417027c399869833912253d115978bfc96a34c
SaliBasri <s.basri@student.tue.nl>
2022-12-30 19:55:03 +0100

    Simple Character Mechanics
    Recreated the player character.
    Added gravity physics.
    Added jump mechanic.
    Removed Object Pick-up.

--- Game_Design_group_16/Assets/Character/Scripts/PlayerMovement.cs
+++ Game_Design_group_16/Assets/Character/Scripts/PlayerMovement.cs
@@ -8,30 +8,36 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
     Vector3 velocity;
-    bool isGrounded;
+    bool isGrounderd;
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        
-        if(isGrounded && velocity.y < 0)
+        isGrounderd = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounderd && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-        
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounderd)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
